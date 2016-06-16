b69a3cddbd9c86a945a0e3319dc4e6163b84ca48
zhangwenan <jquerytech@gmail.com>
2016-06-16 20:22:53 +0800

    outting 临时提交

---
+++ index.js
@@ -0,0 +1,18 @@
+var render = {
+  /**
+   *
+   * @param conf
+   * {
+   *   len: 10, // 长度
+   *   upper: true, // 必须包含大写
+   *   num: true,   // 必须包含数字
+   *   letter: true,  // 必须包含字母
+   *   reg: '(\w)\1(\w)\1\1'    //
+   * }
+   */
+  create: function(conf){
+
+  }
+};
+
+module.exports = render;
